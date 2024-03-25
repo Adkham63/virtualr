ed15da2f6326d16425775653da06db9fd6d6267a
Adkham63 <karimovadham63@gmail.com>
2024-03-25 22:20:14 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import FeatureSection from "./components/FeatureSection";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Workflow from "./components/Workflow";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testimonials />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
